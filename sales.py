6c57b7ce70ba6f4031d1cf56c235ea62b9662afc
ThatsKempo <102661100+ThatsKempo@users.noreply.github.com>
2024-11-21 14:57:42 +0000

    Added comments to sales
    Added comments to sales

--- sales.py
+++ sales.py
@@ -2,9 +2,9 @@ import random
 
 def daily_sales(available_items, inventory_records, current_day):
 
-    if current_day % 7 != 0:
-        sold_units = random.randrange(0,200)
-        available_items = available_items - sold_units
-        inventory_records.append([current_day, sold_units, 0, available_items])
+    if current_day % 7 != 0:    # Checks if day is not equal to 7 (the restock day)
+        sold_units = random.randrange(0,200)    # Creates a random value between 0 and 200 to use at the sold units for the day
+        available_items = available_items - sold_units  # Amends available_items after items have been sold
+        inventory_records.append([current_day, sold_units, 0, available_items]) # Appends the new infomation onto inventory_records
     
     return available_items
