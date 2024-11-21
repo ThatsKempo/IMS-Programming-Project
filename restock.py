109ae24b865157498ae3e283f60f85b1816c70c2
ThatsKempo <102661100+ThatsKempo@users.noreply.github.com>
2024-11-21 15:02:45 +0000

    Added comments to restock
    Added comments to restock

--- restock.py
+++ restock.py
@@ -1,8 +1,8 @@
 def restock_inventory(available_items, inventory_records, current_day):
 
-    if current_day % 7 == 0:
-        restocked_units = (2000-available_items)
-        available_items = 2000
-        inventory_records.append([current_day, 0, restocked_units, available_items])
+    if current_day % 7 == 0:    # Checks if current day is a 7th day of the week
+        restocked_units = (2000-available_items)    # Sees how many items need to be restocked
+        available_items = 2000  # Restocks available_items by setting it to 2000
+        inventory_records.append([current_day, 0, restocked_units, available_items])    # Appends new data to inventory_records
     
     return available_items
